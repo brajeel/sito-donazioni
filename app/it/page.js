9a4ddb05fdf90a9d6860815b6a1a9eddb00693ea
brajeel <brajeel.nolimit@gmail.com>
2026-04-30 12:25:37 +0200

    Update page.js

--- app/it/page.js
+++ app/it/page.js
@@ -1,1 +1,9 @@
-
+export default function HomeIT() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        SITO ITALIANO OK
+      </h1>
+    </main>
+  );
+}
